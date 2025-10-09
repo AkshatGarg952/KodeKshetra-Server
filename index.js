a8110b28485df39f25767da7922acd5866affde4
akshat45 <145849325+AkshatGarg952@users.noreply.github.com>
2025-10-09 18:01:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://kode-kshetra-client.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
@@ -189,7 +189,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://kode-kshetra-client.vercel.app/',
     methods: ['GET', 'POST']
   }
 });
